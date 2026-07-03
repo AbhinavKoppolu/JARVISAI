6e7bb913504be917d656aeb07704b9b6ffddf64b
AbhinavKoppolu <Abhinav.koppolu@gmail.com>
2026-07-02 19:31:35 -0700

    Update jarvis.js

--- jarvis.js
+++ jarvis.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                content: req.body.contents
+                contents: req.body.contents
             })
         });
 
